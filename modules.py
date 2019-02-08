92b0d791b47dcd36a4de900360f25f90007bc423
Bang Tedy <tedysuwega@gmail.com>
2019-02-08 16:42:30 +0700

    skeleton

--- modules.py
+++ modules.py
@@ -3,3 +3,13 @@ from sklearn.naive_bayes import binarize as bn
 def predict(data):
     result = bn(data,0,copy=True)
     return (result)
+
+def fit():
+    return 0
+
+def likelihood():
+    return 0
+
+def posterior():
+    return 0
+
